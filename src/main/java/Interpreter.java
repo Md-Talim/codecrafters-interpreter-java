966ccca53117193df8d76645646672ef64af7124
Mohammad Talim <talimmohammad116@gmail.com>
2025-02-11 16:27:55 +0000

    add support for evaluating relational operators `>`, `>=`, `<`, and `<=`

--- src/main/java/Interpreter.java
+++ src/main/java/Interpreter.java
@@ -67,6 +67,14 @@ class Interpreter implements Expr.Visitor<Object> {
                     return (double) left + (double) right;
                 if (left instanceof String && right instanceof String)
                     return (String) left + (String) right;
+            case GREATER:
+                return (double) left > (double) right;
+            case GREATER_EQUAL:
+                return (double) left >= (double) right;
+            case LESS:
+                return (double) left < (double) right;
+            case LESS_EQUAL:
+                return (double) left <= (double) right;
             default:
                 return null;
         }
